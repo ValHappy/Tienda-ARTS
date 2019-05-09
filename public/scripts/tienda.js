46196cecc71ddf74e095db5bfd1b30e0f5dd9549
Valentina Alegría <val.happy.m@gmail.com>
2019-05-08 23:05:30 -0500

    adelanto estilos carrito y js

--- public/scripts/tienda.js
+++ public/scripts/tienda.js
@@ -1,16 +1,58 @@
+function paginaCargada() {
+
+  var listaProductos = [];
+  var carritoNum = document.querySelector('.carrito__items');
+  if (localStorage.getItem('listaProductos') != null) {
+    listaProductos = JSON.parse(localStorage.getItem('listaProductos'));
+  }
+
+
+  function actualizarCarrito() {
+    carritoNum.innerHTML = listaProductos.length;
+    // localStorage.removeItem('listaProductos');
+    // listaProductos = [];
+  }
+
+  actualizarCarrito();
+
+  var botones = document.querySelectorAll('.item__btn');
+  function recorrerBotones(boton) {
+    function agregarAlCarrito() {
+      var padre = boton.parentNode;
+      var nombre = padre.querySelector('.item__nombre').innerText;
+      var precio = padre.querySelector('.item__precio').innerText;
+      var imagen = padre.querySelector('.item__img').src;
+      var producto = {
+        nombre: nombre,
+        precio: precio,
+        imagen: imagen,
+      };
+
+      listaProductos.push(producto);
+      actualizarCarrito();
+      localStorage.setItem('listaProductos', JSON.stringify(listaProductos));
+    }
+    boton.addEventListener('click', agregarAlCarrito);
+  }
+  botones.forEach(recorrerBotones);
+}
+window.addEventListener('load', paginaCargada);
+
+
+//Drop Down de Categorias
 function desplegar() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
